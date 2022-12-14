41f120055847c2c01568ca7fee17beb6d461a21f
Victor Mendes <victorutdsim7@gmail.com>
2022-12-14 16:06:40 -0300

    feat - requisito 2 [completo]: rota GET /talker/:id

--- src/routes/talker.route.js
+++ src/routes/talker.route.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const { read } = require('../utils/fileSystem');
+const { read, readById } = require('../utils/fileSystem');
 
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
   const talkers = await read('../talker.json');
   return res.status(200).json(talkers);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await readById('../talker.json', id);
+  if (talker) {
+    return res.status(200).json(talker);
+  }
+  return res.status(404).json({
+    message: 'Pessoa palestrante não encontrada',
+  });
 });
 
 module.exports = router;
