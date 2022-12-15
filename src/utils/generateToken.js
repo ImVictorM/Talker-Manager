493e27584ed02e63b728f999f0af8f6fd1bb64cc
Victor Mendes <victorutdsim7@gmail.com>
2022-12-15 19:19:16 -0300

    refactor: função para gerar token

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,7 +2,7 @@ function generateToken() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let token = '';
   const MAX_TOKEN_LENGTH = 16;
-  for (let char = 0; token.length < MAX_TOKEN_LENGTH; char += 1) {
+  while (token.length < MAX_TOKEN_LENGTH) {
     const randomIndex = Math.floor(Math.random() * characters.length);
     token += characters.charAt(randomIndex);
   }
